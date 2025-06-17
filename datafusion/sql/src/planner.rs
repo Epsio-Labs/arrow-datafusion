39cecaae1bcbd499ae19b685fbb86709e673e958
Kobi Grossman <kobi@epsio.io>
2025-06-17 19:22:52 +0300

    planner: add sql json type -> our json type conversion

--- datafusion/sql/src/planner.rs
+++ datafusion/sql/src/planner.rs
@@ -763,8 +763,8 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
                     .collect::<Result<Vec<_>>>()?;
                 Ok(DataType::Struct(Fields::from(fields)))
             }
+            SQLDataType::JSON | SQLDataType::JSONB => Ok(json_type()),
             SQLDataType::Nvarchar(_)
-            | SQLDataType::JSON
             | SQLDataType::Uuid
             | SQLDataType::Binary(_)
             | SQLDataType::Varbinary(_)
@@ -791,7 +791,6 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
             | SQLDataType::Bytes(_)
             | SQLDataType::Int64
             | SQLDataType::Float64
-            | SQLDataType::JSONB
             | SQLDataType::Unspecified
             | SQLDataType::Int16
             | SQLDataType::Int32
